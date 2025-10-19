ef3b67cdaf949fbff73fd7fb0bc718edc7c6d8de
Yahu Baba <123031912+yahoo-baba@users.noreply.github.com>
2025-10-19 12:50:22 +0530

    Todo App - Video Code

---
+++ todo-app/src/Todo.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+function Todo() {
+    const [newTodo, setNewTodo] = useState('')
+    const [todos, setTodos] = useState([])
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(newTodo){
+            setTodos([...todos, {text:newTodo, completed: false }])
+            setNewTodo('')
+        }
+    }
+
+    const handleDelete = (index) =>{
+        const newTodos = [...todos];
+        newTodos[index].completed = !newTodos[index].completed
+        setTodos(newTodos)
+    }
+
+  return (
+    <div>
+        <h1>Todo App</h1>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text"  
+                placeholder='Add new todo'
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+            />
+            <button type='submit'>Add Todo</button>
+        </form>
+        <ul>
+            {todos.map((todo, index) => (
+                <li key={index}>
+                    <span 
+                    style={{ textDecoration : todo.completed ? 'line-through' : 'none' }}>
+                    {todo.text}</span>
+                    <button onClick={() => handleDelete(index)}>Delete</button>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default Todo
