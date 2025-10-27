10782c4dad1f91ee4c4f61a219a398f52db272a8
Yahu Baba <123031912+yahoo-baba@users.noreply.github.com>
2025-10-28 03:56:04 +0530

    Expense Tracker Project - Video Code

---
+++ expense-tracker/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import ExpenseForm from './ExpenseForm'
+import ExpenseList from './ExpenseList'
+import './App.css'
+
+function App() {
+  const [expenses, setExpenses] = useState(() => {
+    const saved = localStorage.getItem("expenses")
+    return saved ? JSON.parse(saved) : [];
+  })
+
+  // Save to LocalStorage whenever expenses change
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses))
+  },[expenses])
+
+  // Add New Expense in Expenses State variable
+  const addExpense = (expense) => {
+    setExpenses((prev) => [...prev, expense] )
+  }
+
+  // Delete Expense
+  const deleteExpense = (id) => {
+    setExpenses((prev) => prev.filter((item) => item.id != id) )
+  }
+
+  const totalExpenses  = expenses.reduce((sum, item) => sum + item.amount, 0)
+
+  return (
+    <div className="app-container">
+      <h1>💰 Expense Tracker</h1>
+      <ExpenseForm onAddExpense={addExpense} />
+      <h3 className="total">Total Expense: ₹{totalExpenses.toFixed(2)}</h3>
+      <ExpenseList expenses={expenses} onDelete={deleteExpense} />
+    </div>
+  )
+}
+
+export default App
---
+++ expense-tracker/src/ExpenseForm.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, useRef } from 'react'
+
+function ExpenseForm({ onAddExpense }) {
+    const [title, setTitle] = useState("")
+    const [amount, setAmount] = useState("")
+    const titleRef = useRef();
+
+    // Function to add new expense and pass to onAddExpense() function
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!title || !amount) return alert("Please fill all fields!")
+
+        const newExpense = {
+            id: Date.now(),
+            title,
+            amount: parseFloat(amount)
+        } 
+        
+        onAddExpense(newExpense)
+        setTitle("")
+        setAmount("")
+        titleRef.current.focus();
+    }
+
+  return (
+    <form className="expense-form" onSubmit={handleSubmit}>
+        <input 
+            placeholder="Expense Title" 
+            type="text" 
+            value={title} 
+            onChange={(e) => setTitle(e.target.value)}
+            ref={titleRef}
+        />
+        <input 
+            placeholder="Amount ₹" 
+            type="number" 
+            value={amount} 
+            onChange={(e) => setAmount(e.target.value)}
+        />
+        <button type="submit">Add Expense</button>
+    </form>
+  )
+}
+
+export default ExpenseForm
